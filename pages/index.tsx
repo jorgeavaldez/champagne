cee2f43b25aadd7b023736efdb3afc400e0eb48d
Jorge Valdez <valdez.a.jorge@gmail.com>
2021-09-04 23:46:44 -0500

    remove index css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 import Link from "next/link";
 import { useNear } from "../src/near/hooks";
 
-// import styles from "../styles/pages/index.module.css";
-
 function HomePage() {
+
   const { signedIn, accountId, networkId } = useNear();
 
   return (
-    <main className={styles.main}>
-      <header className={styles.header}>
+    <main>
+      <header>
         <h1>champagne</h1>
         <p>
           create campaigns to reward twitter interactions with a near droplink
