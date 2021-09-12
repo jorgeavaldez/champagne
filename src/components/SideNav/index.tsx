dc86c595b69b5a33a014dcdd944d68e5dc536e01
Amanda  Esposito <amandaesposito@Amandas-MBP-3.attlocal.net>
2021-09-12 13:36:12 -0500

    Added sections to side nav

--- src/components/SideNav/index.tsx
+++ src/components/SideNav/index.tsx
@@ -4,7 +4,7 @@ import { useNear } from "../../near/hooks";
 
 import styles from "./SideNav.module.css";
 
-import { Nav } from "react-bootstrap";
+import { Container, Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
 
 
 export default function Sidebar() {
@@ -17,38 +17,52 @@ export default function Sidebar() {
         });
     };
 
+    const goToNearWallet = () => {
+       //TODO: go to near wallet
+    };
+
     return (
-        <Nav className={`bg-dark ${styles.sidebar}`}
-            activeKey="/home"
-            onSelect={selectedKey => alert(`selected ${selectedKey}`)}
-        >
-            <div className="sidebar-sticky"></div>
-            <Nav.Item>
-                <Nav.Link href="/home">Active</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-1">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-2">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="disabled" disabled>
-                    Disabled
-                </Nav.Link>
-            </Nav.Item>
-        </Nav>
+        <Navbar bg="dark" variant="dark" expand="lg" className={styles.sidebar}>
+            <Container className="flex-column h-100">
+                {/* TODO: add login user name and link to wallet */}
+                <Navbar.Brand>
+                    <Button variant="dark" className="w-100" onClick={goToNearWallet}>
+                        charfield.near
+                    </Button>
+                </Navbar.Brand>
 
-        // <div className={styles.container}>
-        //   <div className={styles.buttons}>
-        //     <div className={styles.logout} onClick={onLogoutClick}>
-        //       ⁂ logout
-        //     </div>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-        //     <Link href="/" prefetch>⁂ home</Link>
+                <Navbar.Collapse className="flex-column" id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavDropdown title="Campaigns" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/Campaign/Active">Active Campaigns</NavDropdown.Item>
+                            <NavDropdown.Item href="/Campaign/New">New Campaign</NavDropdown.Item>
+                            <NavDropdown.Item href="/Campaign/Calender">Campaign Calender</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/Campaign/All">View All Campaigns</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav className="me-auto">
+                        <NavDropdown title="Analytics" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/Analytics/Reports">Reports</NavDropdown.Item>
+                            <NavDropdown.Item href="/Analytics/Leads">Leads</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav> 
+                    <Nav className="me-auto">
+                        <NavDropdown title="Social Accounts" id="basic-nav-dropdown">
+                            {/* TODO: add login and logout capablities when clicked */}
+                            <NavDropdown.Item href="">Instagram</NavDropdown.Item>
+                            <NavDropdown.Item href="">Twitter</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
 
-        //     <Link href="/campaign/list">⁂ campaigns</Link>
-        //   </div>
-        // </div>
+                <Button variant="dark" className="w-100" onClick={onLogoutClick}>
+                    logout
+                </Button>
+                
+            </Container>
+        </Navbar>
     );
 }
