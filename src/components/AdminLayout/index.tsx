419269f7219aba551b2d51fd1e5a99102c5eb39f
Amanda  Esposito <amandaesposito@Amandas-MBP-3.attlocal.net>
2021-09-13 00:13:09 -0500

    added media queries for side nav and top nav

--- src/components/AdminLayout/index.tsx
+++ src/components/AdminLayout/index.tsx
@@ -1,20 +1,21 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { useMediaQuery } from "react-responsive";
 
 import Sidebar from '../SideNav';
+import TopNav from '../TopNav';
 import AdminHeader from '../AdminHeader';
 import AdminPageContainer from "../AdminPageContainer";
 
-import styles from "./AdminLayout.module.css";
-
 export default function Layout({ children }) {
+
+    const isSmallerThanIpad = useMediaQuery({ query: '(max-width: 768px)' });
+
     return (
         <Container fluid>
-            <Row>
-                <Col lg={2} id="sidebar-wrapper">
-                    <Sidebar />
-                </Col>
-                <Col lg={10} id="page-content-wrapper">
-                    <AdminHeader/>
+            <Row> 
+                {isSmallerThanIpad ? <TopNav/> : <Col lg={2} id="sidebar-wrapper"><Sidebar/> </Col>}
+                <Col lg={10} md={12} id="page-content-wrapper" className="mt-lg-0 mt-3">
+                    {!isSmallerThanIpad && <AdminHeader />}
                     <AdminPageContainer title="Welcome Back">
                         {children}
                     </AdminPageContainer>
